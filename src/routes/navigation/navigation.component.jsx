a006538c80678f7dd0f6079ee17f70df3ecfdfd2
Radoslaw <125380891+Rado-Codes@users.noreply.github.com>
2023-07-02 19:19:46 +0200

    styled components of navigation

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -10,7 +10,12 @@ import { CartContext } from '../../contexts/cart.context';
 
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 
-import './navigation.styles.scss';
+import {
+	NavigationContainer,
+	NavLinks,
+	NavLink,
+	LogoContainer,
+} from './navigation.styles';
 
 function Navigation() {
 	const { currentUser } = useContext(UserContext);
@@ -22,27 +27,23 @@ function Navigation() {
 
 	return (
 		<>
-			<div className='navigation'>
-				<Link className='logo-container' to='/'>
+			<NavigationContainer>
+				<LogoContainer to='/'>
 					<CrownLogo className='logo' />
-				</Link>
-				<div className='nav-links-container'>
-					<Link className='nav-link' to='/shop'>
-						SHOP
-					</Link>
+				</LogoContainer>
+				<NavLinks>
+					<NavLink to='/shop'>SHOP</NavLink>
 					{currentUser ? (
-						<span className='nav-link' onClick={signOutHandler}>
+						<NavLink as='span' onClick={signOutHandler}>
 							SIGN OUT
-						</span>
+						</NavLink>
 					) : (
-						<Link className='nav-link' to='/auth'>
-							SIGN IN
-						</Link>
+						<NavLink to='/auth'>SIGN IN</NavLink>
 					)}
 					<CartIcon />
-				</div>
+				</NavLinks>
 				{isCartOpen && <CartDropdown />}
-			</div>
+			</NavigationContainer>
 			<Outlet />
 		</>
 	);
