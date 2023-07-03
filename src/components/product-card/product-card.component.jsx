2082d7a5402c55f3859d8ca138d19b3be3f4176a
Radoslaw <125380891+Rado-Codes@users.noreply.github.com>
2023-07-03 09:13:10 +0200

    added Button component based on classes

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -3,7 +3,7 @@ import { CartContext } from '../../contexts/cart.context';
 
 import './product-card.styles.scss';
 
-import Button from '../button/button.component';
+import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
 
 function ProductCard({ product }) {
 	const { name, price, imageUrl } = product;
@@ -21,7 +21,10 @@ function ProductCard({ product }) {
 				<span className='name'>{name}</span>
 				<span className='price'>{price}</span>
 			</div>
-			<Button onClick={addProductToCart} buttonType='inverted'>
+			<Button
+				onClick={addProductToCart}
+				buttonType={BUTTON_TYPE_CLASSES.inverted}
+			>
 				Add to card
 			</Button>
 		</div>
--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,10 +1,9 @@
 import { useState } from 'react';
 
 import FormInput from '../form-input/form-input.component';
-import Button from '../button/button.component';
+import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
 
 import {
-	createUserDocumentFromAuth,
 	signInWithGooglePopup,
 	signInAuthUserWithEmailandPassword,
 } from '../../utils/firebase/firebase.utils';
@@ -82,7 +81,7 @@ function SignInForm() {
 					<Button type='submit'>Sign In</Button>
 					<Button
 						type='button'
-						buttonType='google'
+						buttonType={BUTTON_TYPE_CLASSES.google}
 						onClick={signInWithGoogle}
 					>
 						Google sign in
