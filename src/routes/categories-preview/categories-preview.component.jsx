cf795cb6d949e11d5d8322d85d235deb36739b71
Radoslaw <125380891+Rado-Codes@users.noreply.github.com>
2023-07-15 14:33:30 +0200

    feat: spinner based on isLoading added to Category and CategoryPreview

--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -2,24 +2,33 @@ import { Fragment } from 'react';
 import { useSelector } from 'react-redux';
 
 import CategoryPreview from '../../components/category-preview/category-preview.compononet';
+import { Spinner } from '../../components/spinner/spinner.component';
 
-import { selectCategoriesMap } from '../../store/categories/category.selector';
+import {
+	selectCategoriesMap,
+	selectCategoriesIsLoading,
+} from '../../store/categories/category.selector';
 
 function CategoriesPreview() {
 	const categoriesMap = useSelector(selectCategoriesMap);
+	const isLoading = useSelector(selectCategoriesIsLoading);
 
 	return (
 		<Fragment>
-			{Object.keys(categoriesMap).map((title) => {
-				const products = categoriesMap[title];
-				return (
-					<CategoryPreview
-						key={title}
-						products={products}
-						title={title}
-					/>
-				);
-			})}
+			{isLoading ? (
+				<Spinner />
+			) : (
+				Object.keys(categoriesMap).map((title) => {
+					const products = categoriesMap[title];
+					return (
+						<CategoryPreview
+							key={title}
+							products={products}
+							title={title}
+						/>
+					);
+				})
+			)}
 		</Fragment>
 	);
 }
--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -3,13 +3,18 @@ import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
 import ProductCard from '../../components/product-card/product-card.component';
-import { selectCategoriesMap } from '../../store/categories/category.selector';
+import { Spinner } from '../../components/spinner/spinner.component';
+import {
+	selectCategoriesMap,
+	selectCategoriesIsLoading,
+} from '../../store/categories/category.selector';
 
 import { CategoryContainer, CategoryTitle } from './category.styles';
 
 function Category() {
 	const { category } = useParams(); //accessing variable from url
 	const categoriesMap = useSelector(selectCategoriesMap);
+	const isLoading = useSelector(selectCategoriesIsLoading);
 	const [products, setProducts] = useState(categoriesMap[category]);
 
 	useEffect(() => {
@@ -19,14 +24,21 @@ function Category() {
 	return (
 		<>
 			<CategoryTitle>{category.toUpperCase()}</CategoryTitle>
-			<CategoryContainer>
-				{products &&
-					products.map((product) => {
-						return (
-							<ProductCard key={product.id} product={product} />
-						);
-					})}
-			</CategoryContainer>
+			{isLoading ? (
+				<Spinner />
+			) : (
+				<CategoryContainer>
+					{products &&
+						products.map((product) => {
+							return (
+								<ProductCard
+									key={product.id}
+									product={product}
+								/>
+							);
+						})}
+				</CategoryContainer>
+			)}
 		</>
 	);
 }
