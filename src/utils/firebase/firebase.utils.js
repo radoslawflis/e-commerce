9a07c53878bb353633ed2ec9e550941190deef50
Radoslaw <125380891+Rado-Codes@users.noreply.github.com>
2023-07-19 12:29:44 +0200

    feat: changed observable onAuthStateChangedListener into Promise

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -146,3 +146,16 @@ export const signOutUser = async () => signOut(auth);
 
 export const onAuthStateChangedListener = (callback) =>
 	onAuthStateChanged(auth, callback);
+
+export const getCurrentUser = () => {
+	return new Promise((resolve, reject) => {
+		const unsubscribe = onAuthStateChanged(
+			auth,
+			(userAuth) => {
+				unsubscribe();
+				resolve(userAuth);
+			},
+			reject
+		);
+	});
+};
